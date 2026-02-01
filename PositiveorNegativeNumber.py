23e7d433b5be0fabb78068ce8959fede5b35f96b
Sahil Burle <sahilburle1998@gmail.com>
2026-02-01 22:49:03 +0530

    Create PositiveorNegativeNumber.py

---
+++ PositiveorNegativeNumber.py
@@ -0,0 +1,10 @@
+print("Enter the number : ")
+
+num = int(input())
+
+if num < 0:
+    print("Number is negative")
+elif num > 0:
+    print("Number is positive")
+else:
+    print("Number is zero")
