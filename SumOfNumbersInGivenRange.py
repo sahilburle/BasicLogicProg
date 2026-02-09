ff00ca9a88e8457d905f0a2ea9839b0101646b4a
Sahil Burle <sahilburle1998@gmail.com>
2026-02-09 21:24:56 +0530

    in specific range

---
+++ SumOfNumbersInGivenRange.py
@@ -0,0 +1,11 @@
+abc = 0
+
+print ("Enter the value of m :")
+m = int(input())
+
+print ("Enter the value of n :")
+n = int(input())
+
+for total in range (m,n) :
+    abc = abc + total
+print (abc)
