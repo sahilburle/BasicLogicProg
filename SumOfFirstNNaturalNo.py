e2c6dd092d1e55173bdab47b867a4f041894af40
Sahil Burle <sahilburle1998@gmail.com>
2026-02-09 21:06:16 +0530

    some additional way of executing

--- SumOfFirstNNaturalNo.py
+++ SumOfFirstNNaturalNo.py
@@ -8,3 +8,21 @@ for total in range (1,n) :
     abc = abc + total
 print (abc)
 
+"""
+other below ways
+
+num = 5
+print(int(num*(num+1)/2))
+...........................
+
+
+def getSum(num):
+  if num == 1:
+    return 1
+  return num + getSum(num-1)
+
+num = 5
+print(getSum(num))
+
+
+"""
