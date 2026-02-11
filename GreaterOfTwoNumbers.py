d5630472a8e32b0cdedbb5b1772d0391b4707cf8
Sahil Burle <sahilburle1998@gmail.com>
2026-02-11 17:54:57 +0530

    greater number

---
+++ GreaterOfTwoNumbers.py
@@ -0,0 +1,11 @@
+print("Please enter the value of a and b :")
+
+a = int(input())
+b = int(input())
+
+if (a > b) :
+    print(f"{a} is greater")
+elif (a == b) :
+    print("Both are same numbers")
+else :
+    print(f"{b} is greater")
